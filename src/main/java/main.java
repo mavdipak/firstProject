5288f79d5ba6954df057d77deb2954a0aab9c546
dipakmav <mavdipak@gmail.com>
2017-03-28 04:52:58 +0530

    added person class

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,8 +1,14 @@
+import org.firstProject.Person;
+
 /**
  * Created by dipakk on 3/27/2017.
  */
 public class main {
     public static void main(String args[]){
-        System.out.println("hi wassup");
+
+        Person p1 = new Person();
+        p1.name = "name doesn't define you";
+        p1.email = "it requires to IT guy";
+        System.out.println("hi wassup "+p1.email+" "+p1.name);
     }
 }
---
+++ src/main/java/org/firstProject/Person.java
@@ -0,0 +1,10 @@
+package org.firstProject;
+
+/**
+ * Created by dipakk on 3/28/2017.
+ */
+public class Person {
+    public String name="dipak";
+    public String email="mavdipak@gmail.com";
+
+}
